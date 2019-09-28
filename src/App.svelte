f2a27e3e119e42ce3884da13f8bae434db7e192c
Ben Godfrey <bcgodfrey91@gmail.com>
2019-09-27 22:58:42 -0500

    add initial idea box functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script>
+	let ideas = [];
+	const addIdea = (e) => {
+		ideas = [
+			...ideas,
+			{
+				title: e.target[0].value,
+				content: e.target[1].value
+			}
+		]
+	}
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
 </style>
 
-<form>
+<form on:submit|preventDefault={addIdea}>
 	<input />
 	<textarea />
 	<button>Post Idea</button>
 </form>
+
+<ul>
+	{#each ideas as idea}
+		<li>
+			<h1>{idea.title}</h1>
+			<span>{idea.content}</span>
+		</li>
+	{/each}
+</ul>
