4cab2340bad3ab9e7a57ad035913bc366d155d86
Ben Godfrey <bcgodfrey91@gmail.com>
2019-09-28 23:48:29 -0500

    add remove idea functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,23 @@
 <script>
 	let ideas = [];
+	let count = 0;
+
 	const addIdea = (e) => {
 		ideas = [
 			...ideas,
 			{
 				title: e.target[0].value,
-				content: e.target[1].value
+				content: e.target[1].value,
+				id: count
 			}
-		]
+		];
+		count++;
+	};
+
+	const removeIdea = (i) => {
+		ideas = ideas.filter((idea, index) => i !== index);
 	}
+
 </script>
 
 <style>
@@ -21,10 +30,11 @@
 </form>
 
 <ul>
-	{#each ideas as idea}
+	{#each ideas as idea, i (idea.id)}
 		<li>
 			<h1>{idea.title}</h1>
 			<span>{idea.content}</span>
+			<button on:click={() => removeIdea(i)}>Delete Idea</button>
 		</li>
 	{/each}
 </ul>
